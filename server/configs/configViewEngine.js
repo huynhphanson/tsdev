7ba9d09ee4bf49b5d7ce4fa5c741bb99a810c827
huynhphanson <huynhphantuysonfreelance@gmail.com>
2025-06-01 22:02:31 +0700

    change config public v3

--- server/configs/configViewEngine.js
+++ server/configs/configViewEngine.js
@@ -18,7 +18,6 @@ export function configViewEngine(app) {
   app.set('view engine', 'ejs');
   app.set('views', path.join(__dirname, '..', 'views'));
 
-  // app.use(express.static(path.join(__dirname, '..', 'public')));
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, '..', 'public')));
 
 }
